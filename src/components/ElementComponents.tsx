7ff21a7041ca86b8bbb2759dc5921a9aacd3bdaa
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-09-30 23:11:17 +0800

    Added searchbar to Element and Components list

--- src/components/ElementComponents.tsx
+++ src/components/ElementComponents.tsx
@@ -1,12 +1,13 @@
 import {defineComponent, ElementComponent} from "@/core";
 import "@/assets/ElementComponents.scss"
 import DragStartEvent = JQuery.DragStartEvent;
-import React from "react";
+import React, {useState} from "react";
+import {it} from "node:test";
 
-interface itemProps{
+interface itemProps {
     name: string,
     desc: string,
-    attrs?: {[attrName:string] : string}
+    attrs?: { [attrName: string]: string }
 }
 
 const ElementComponentItem = defineComponent<itemProps>((props, context) => {
@@ -17,9 +18,9 @@ const ElementComponentItem = defineComponent<itemProps>((props, context) => {
             return
         }
 
-        ev.dataTransfer.setData("tagName",props.name);
+        ev.dataTransfer.setData("tagName", props.name);
         if (props.attrs) {
-            ev.dataTransfer.setData("attrs",JSON.stringify(props.attrs))
+            ev.dataTransfer.setData("attrs", JSON.stringify(props.attrs))
         }
 
 
@@ -34,24 +35,47 @@ const ElementComponentItem = defineComponent<itemProps>((props, context) => {
     )
 })
 
-interface listProps{
-    elementComponents:ElementComponent[]
+interface listProps {
+    elementComponents: ElementComponent[]
 }
 
 export default defineComponent<listProps>((props, context) => {
-    return (
-        <ul className={"el-comp-list"}>
-            {
-                props.elementComponents.map((value, index) => {
-                    return (
-                        <ElementComponentItem
-                            key={index}
-                            name={value.name}
-                            desc={value.description??""}
-                        />
-                    )
-                })
+    const [items, setItems] = useState(props.elementComponents)
+
+    function OnSearchUpdate(e: React.ChangeEvent<HTMLInputElement>) {
+        let searchQuery = e.target.value
+        let filteredItems: ElementComponent[] = []
+
+        for (let i = 0; i < props.elementComponents.length; i++) {
+            let item = props.elementComponents[i];
+            if (item.name.includes(searchQuery)) {
+                filteredItems.push(item)
+            } else if (item.description) {
+                if (item.description.includes(searchQuery)) {
+                    filteredItems.push(item)
+                }
             }
-        </ul>
+        }
+
+        setItems(filteredItems)
+    }
+
+    return (
+        <div className={"el-comp-ctn"}>
+            <input className={"el-comp-search"} placeholder={"Search name or description"} type={"search"} onChange={OnSearchUpdate}/>
+            <ul className={"el-comp-list"}>
+                {
+                    items.map((value, index) => {
+                        return (
+                            <ElementComponentItem
+                                key={index}
+                                name={value.name}
+                                desc={value.description ?? ""}
+                            />
+                        )
+                    })
+                }
+            </ul>
+        </div>
     )
 })

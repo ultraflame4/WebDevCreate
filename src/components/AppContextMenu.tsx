c2a34e3265a41b6d82ef8fb55028493b78c87e5a
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-10-14 14:51:28 +0800

    context menu has been added for list item (in past commits, this is just a stud)

--- src/components/AppContextMenu.tsx
+++ src/components/AppContextMenu.tsx
@@ -76,7 +76,6 @@ export const AppContextMenu = defineComponent((props, context) => {
             else{
                 isMenuJustOpen.current=false
             }
-
         }
 
         document.addEventListener("mousemove", mouseMove)
